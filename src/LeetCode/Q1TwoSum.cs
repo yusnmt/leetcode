c09ca3b2dff0f99964ca44a4725bf229efff3091
Yusnmt <yusnmt@YusnmtnoMacBook-Pro.local>
2025-12-24 16:38:32 +0900

    add: LeetCode project and TwoSum problem and solution.

---
+++ src/LeetCode/Q1TwoSum.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode;
+
+public static class Q1TwoSum
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>(nums.Length);
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (map.TryGetValue(complement, out var index))
+            {
+                return [index, i];
+            }
+
+            if (!map.ContainsKey(nums[i]))
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        return [];
+    }
+}
---
+++ tests/LeetCode.Test/Q1TwoSumTest.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Test;
+
+public class Q1TwoSumTest
+{
+    [TestCase(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [TestCase(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+
+    public void TwoSum(int[] nums, int target, int[] expected)
+    {
+        var answer = Q1TwoSum.TwoSum(nums, target);
+        Assert.That(answer, Is.EqualTo(expected));
+    }
+}
