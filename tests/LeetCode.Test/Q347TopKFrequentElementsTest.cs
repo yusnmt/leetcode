ca9c96111903723d4c7cc2a83d5d119de5b27027
yusnmt <yusnmt@gmail.com>
2025-12-31 18:02:40 +0900

    add: TopKFrequentElements problem and solution.

---
+++ tests/LeetCode.Test/Q347TopKFrequentElementsTest.cs
@@ -0,0 +1,26 @@
+using LeetCode.Q347TopKFrequentElements;
+
+namespace LeetCode.Test;
+
+public class Q347TopKFrequentElementsTest
+{
+    [TestCase(new[] { 1, 1, 1, 2, 2, 3 }, 2, new[] { 1, 2 })]
+    [TestCase(new[] { 1 }, 1, new[] { 1 })]
+    [TestCase(new[] { 1, 2, 1, 2, 1, 2, 3, 1, 3, 2 }, 2, new[] { 1, 2 })]
+
+    public void TopKFrequent_PriorityQueue(int[] nums, int k, int[] expected)
+    {
+        var answer = Solution.TopKFrequent_PriorityQueue(nums, k);
+        Assert.That(answer, Is.EquivalentTo(expected));
+    }
+    
+    [TestCase(new[] { 1, 1, 1, 2, 2, 3 }, 2, new[] { 1, 2 })]
+    [TestCase(new[] { 1 }, 1, new[] { 1 })]
+    [TestCase(new[] { 1, 2, 1, 2, 1, 2, 3, 1, 3, 2 }, 2, new[] { 1, 2 })]
+
+    public void TopKFrequent_BucketsSort(int[] nums, int k, int[] expected)
+    {
+        var answer = Solution.TopKFrequent_BucketsSort(nums, k);
+        Assert.That(answer, Is.EquivalentTo(expected));
+    }
+}
